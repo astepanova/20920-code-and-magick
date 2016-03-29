54e3c5b46ff491fe7cd700cab75d189f8a7f9813
astepanova <anya.stepanova@mail.ru>
2016-03-30 01:01:50 +0300

    Модуль 1 задание 2

---
+++ build/js/check.js
@@ -0,0 +1,34 @@
+function getMessage(a, b) {
+
+  if (typeof a == 'boolean') {
+
+    if (a == true) {
+      return ( 'Я попал в ' + b );
+    }
+    return ( 'Я никуда не попал' );
+
+  }
+
+  if (typeof a == 'number') {
+    return ( 'Я прыгнул на ' + a * 100 + ' сантиметров' );
+  }
+
+  if (a.constructor === Array) {
+
+    if (( b ) && (b.constructor === Array)) {
+      var s = 0;
+      for (var i = 0; i < a.length; i++) {
+        s += a[i] * b[i];
+      }
+      return ('Я прошёл ' + s + ' метров');
+    }
+
+    s = 0;
+    for (i = 0; i < a.length; i++) {
+      s += a[i];
+    }
+    return ('Я прошёл ' + s + ' шагов');
+
+  }
+
+}
