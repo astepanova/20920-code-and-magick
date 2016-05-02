6ed4ae129b7e847f047dc3e6c9c850a41e5e6df1
astepanova <anya.stepanova@mail.ru>
2016-05-02 22:50:38 +0300

    Модуль 5 задание1

--- src/reviews.js
+++ src/reviews.js
@@ -3,6 +3,16 @@ var filter = document.querySelector('.reviews-filter');
 filter.classList.add('invisible');
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.getElementById('review-template');
+var reviews = [];
+var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
+var Filter = {
+  'ALL': 'reviews-all',
+  'RECENT': 'reviews-recent',
+  'GOOD': 'reviews-good',
+  'BAD': 'reviews-bad',
+  'POPULAR': 'reviews-popular'
+};
+var DEFAULT_FILTER = Filter.ALL;
 // Клонирование содержимого шаблона
 var elementToClone;
 if ('content' in templateElement) {
@@ -15,7 +25,6 @@ var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
   element.querySelector('.review-rating').textContent = data.rating;
   element.querySelector('.review-text').textContent = data.description;
-  container.appendChild(element);
   var avatarLoadTimeout;
   var avatar = new Image();
   // Если фотография загрузилась
@@ -37,8 +46,95 @@ var getReviewElement = function(data, container) {
     avatar.src = '';
     element.classList.add('review-load-failure');
   }, IMAGE_TIMEOUT);
+  container.appendChild(element);
 };
-window.reviews.forEach(function(review) {
-  getReviewElement(review, reviewsContainer);
+// Получение данных
+var getReviews = function(callback) {
+  var xhr = new XMLHttpRequest();
+  var reviewsSection = document.querySelector('.reviews');
+  reviewsSection.classList.add('reviews-list-loading');
+  xhr.onload = function(evt) {
+    reviewsSection.classList.remove('reviews-list-loading');
+    filter.classList.remove('invisible');
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  };
+  xhr.onerror = function() {
+    reviewsSection.classList.remove('reviews-list-loading');
+    reviewsSection.classList.add('reviews-load-failure');
+  };
+  xhr.timeout = 10000;
+  xhr.ontimeout = function() {
+    reviewsSection.classList.remove('reviews-list-loading');
+    reviewsSection.classList.add('reviews-load-failure');
+  };
+  xhr.open('GET', REVIEWS_LOAD_URL);
+  xhr.send();
+};
+// Создание локальной переменной
+var renderReviews = function(reviews) {
+  reviewsContainer.innerHTML = '';
+  reviews.forEach(function(review) {
+    getReviewElement(review, reviewsContainer);
+  });
+};
+
+// Фильтрация
+var getFilteredReviews = function(reviews, filter) {
+  var reviewsToFilter = reviews.slice(0);
+
+  switch (filter) {
+    case Filter.RECENT:
+      reviewsToFilter.sort(function(a, b) {
+        var date1 = +new Date(a.date);
+        var date2 = +new Date(b.date);
+        return date2 - date1;
+      });
+      break;
+    case Filter.GOOD:
+      reviewsToFilter.filter(function(review) {
+        return review.rating >= 3;
+      }).sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+    case Filter.BAD:
+      reviewsToFilter.filter(function(review) {
+        return review.rating < 3;
+      }).sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+      break;
+    case Filter.POPULAR:
+      reviewsToFilter.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+      break;
+  }
+
+  return reviewsToFilter;
+};
+
+var setFilterEnabled = function(filter) {
+  var filteredReviews = getFilteredReviews(reviews, filter);
+  renderReviews(filteredReviews);
+};
+
+
+// Подключение фильтров
+var setFiltrationEnabled = function() {
+  var reviewsFilter = document.querySelector('.reviews-filter');
+  var filters = reviewsFilter.elements['reviews'];
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function() {
+      setFilterEnabled(this.id);
+    };
+  }
+};
+
+getReviews(function(loadedReviews) {
+  reviews = loadedReviews;
+  setFiltrationEnabled();
+  setFilterEnabled(DEFAULT_FILTER);
 });
-filter.classList.remove('invisible');
+
